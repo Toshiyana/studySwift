32a8903a0825d600061ee62d0443236b52596f50
Toshiki Yanagimoto <toshiyana0306@gmail.com>
2021-03-16 17:15:32 +0900

    add userDefault

---
+++ practiceSwift/practice_userDefault.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+//userDefaultを宣言
+let defaults = UserDefaults.standard
+
+//localにdataの保存
+defaults.set(0.24, forKey: "Volume")
+defaults.set(true, forKey: "MusicOn")
+defaults.set("Taro", forKey: "PlayerName")
+defaults.set(Date(), forKey: "AppLastOpenedByUser")
+let array = [1,2,3]
+defaults.set(array, forKey: "MyArray")
+let dictionary = ["name": "Taro"]
+defaults.set(dictionary, forKey: "dictionaryKey")
+
+//dataの取り出し
+let volume = defaults.float(forKey: "Volume")
+let music = defaults.bool(forKey: "MusicOn")
+let player = defaults.object(forKey: "PlayerName") as! String//objectは返り値Any?
+let appLastOpenedByUser = defaults.object(forKey: "AppLastOpenedByUser")
+let myArray = defaults.array(forKey: "MyArray") as! [Int]
+let myDictionary = defaults.dictionary(forKey: "dictionaryKey")
